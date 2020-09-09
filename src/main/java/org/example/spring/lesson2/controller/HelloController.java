bac8f4b1da0d476387cadec5c86c75295f44f8db
Kirill Kuznetsov <kuznechik.86@mail.ru>
2020-09-09 15:32:57 +0300

    Для удаления метод сделал. Сделал стринг с нужной ссылкой для удаления. Но не хватает хватает знаний html чтобы сделать hyperlink

--- src/main/java/org/example/spring/lesson2/controller/HelloController.java
+++ src/main/java/org/example/spring/lesson2/controller/HelloController.java
@@ -1,14 +1,15 @@
-package org.example.spring.lesson2;
-
+package org.example.spring.lesson2.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
-    @GetMapping("/")
+
+    @GetMapping({"/"})
     public String hello(Model model) {
         model.addAttribute("message", "Hello World");
         return "hello";
     }
 }
+
